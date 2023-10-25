358aa6e37abc56d107b8a5fa492a9398d800b56d
John Ferguson <jaf13@johns-mac-mini.mynet>
2023-10-25 21:58:20 +0100

    refactored eval

--- src/spl/mod.rs
+++ src/spl/mod.rs
@@ -5,10 +5,8 @@
 //! 
 
 
-use crate::utl;
-
-use float_cmp::approx_eq;
-use nalgebra as nl;
+use approx::ulps_eq;
+use nalgebra as na;
 
 
 // ------------------------------------------- Structs ------------------------------------------ //
@@ -32,26 +30,26 @@ pub struct BsplineBasis
 /// This is the maximum allowable order of a bspline basis. It is an arbitrary number.
 const PMAX: usize = 8;
 /// This is the tolerance with which two knots are considered equal
-const KNOT_ULPS: i64 = 256;
+const KNOT_ULPS: u32 = 32;
 
 // --------------------------------------- Free Functions---------------------------------------- //
 
 /// Tolerant less-thant for knots
 fn knot_lt(u1: f64, u2: f64) -> bool 
 {
-    u1 < u2 || approx_eq!(f64, u1, u2, ulps = KNOT_ULPS) 
+    u1 < u2 || ulps_eq!(u1, u2, max_ulps = KNOT_ULPS) 
 }
 
 /// Tolerant greater-thant for knots
 fn knot_gt(u1: f64, u2: f64) -> bool 
 {
-    u1 > u2 || approx_eq!(f64, u1, u2, ulps = KNOT_ULPS) 
+    u1 > u2 || ulps_eq!(u1, u2, max_ulps = KNOT_ULPS) 
 }
 
 /// Tolerant equal for knots
 fn knot_eq(u1: f64, u2: f64) -> bool 
 {
-    approx_eq!(f64, u1, u2, ulps = KNOT_ULPS) 
+    ulps_eq!(u1, u2, max_ulps = KNOT_ULPS) 
 }
 
 fn upper_bound(arr: &[f64], value: f64) -> usize 
@@ -104,7 +102,7 @@ impl BsplineBasis
         let pi = p as i32;
 
         let start = std::cmp::max(0, span_i - pi) as usize;
-        let end = std::cmp::min(span_i, mi - 2 - pi) as usize;
+        let end = (std::cmp::min(span_i, mi - 2 - pi)+1) as usize;
         let num_basis = end - start;
         (start, end, num_basis)
     }
@@ -112,34 +110,131 @@ impl BsplineBasis
 
     pub fn eval(&self, u: f64, p: usize, shape_funs: &mut [f64])
     {
+        assert!(shape_funs.len() >= p+1, "Buffer too small to hold results");
+        assert!(self.is_member(u), "u is outside of ");
         shape_funs.fill(0.0);
         shape_funs[0] = 1.0;
+
+        let mut left = [0.0; PMAX+1];
+        let mut right =[0.0; PMAX+1];
+
         let i = self.find_span(u, p);
-        let (mut saved, mut temp) = (0.0, 0.0);
-        let (mut left, mut right) = ([0.0; PMAX], [0.0; PMAX]);    
 
         for j in 1..p+1
         {
-            saved = 0.0;
-            left[j] = u - self.knots[i + 1 - j];
-            right[j] = self.knots[i + j] - u;
+            left[j-1] = u - self.knots[i + 1 - j];
+            right[j-1] = self.knots[i + j] - u;
+        }
 
+        for j in 1..p+1
+        {
+            let mut saved = 0.0;
             for r in 0..j 
             {
-                temp = shape_funs[r] / (right[r+1] + left[j-r]);
-                shape_funs[r] = saved + (right[r+1] * temp);
-                saved = left[j - r] * temp;
+                let temp = shape_funs[r] / (right[r] + left[j-r-1]);
+                shape_funs[r] = saved + (right[r] * temp);
+                saved = left[j - r - 1] * temp;
             }
             shape_funs[j] = saved;
         }
     }
     //..............................................................................................
 
+
+
     pub fn eval_diff(&self, u: f64, p: usize, n: usize, shape_ders: &mut [f64])
     {
         assert!(self.is_member(u), "u is not member of parameter range");
         assert!(p <= PMAX, "order exceeds max allowable");
         assert!(n <= p, "order of derivative exceeds order");
+        assert!(shape_ders.len() >= (p+1)*(n+1), "Derivative buffer too small");
+
+        shape_ders.fill(0.0);
+        let mut shape_ders_mat = na::DMatrixViewMut::from_slice(shape_ders, n+1, p+1);
+
+        let mut ndu_buf = [0.0; (PMAX+1) * (PMAX+1)];
+        let mut ndu = na::DMatrixViewMut::from_slice(&mut ndu_buf, p+1, p+1);
+
+
+        // ................... compute knot differences  ................... //
+        let sp = self.find_span(u, p);
+        let (mut saved, mut temp) = (0.0, 0.0);
+        let mut left = [0.0; PMAX + 1];
+        let mut right = [0.0; PMAX + 1];
+
+        /* doc: explanation of ndu
+        Upper triangle of ndu is the shape functions, so ndu[(0, 0)]
+        contains N_{0,0}, ndu[(0, 1)] = N{0, 1}, ndu[(1, 1)] = N{1, 1} 
+        and so on. 
+
+        Lower triagle of ndu are the knot differences, so 
+        ndu[(1,0)] = u_{i+1} - u_{i}
+        ndu[(2,0)] = u_{i+1} - 
+        */
+        ndu[(0, 0)] = 1.0;
+
+        for j in 1..p+1 
+        {
+            saved = 0.0;
+            left[j] = u - self.knots[sp + 1 - j];
+            right[j] = self.knots[sp + j] - u;
+
+            for r in 0..j
+            {
+                ndu[(j, r)] = right[r + 1] + left[j - r];
+                temp = ndu[(r, j - 1)] / ndu[(j, r)];
+
+                ndu[(r, j)] = saved + (right[r+1] * temp);
+                saved = left[j - r] * temp;
+            }
+            ndu[(j, j)] = saved;
+        }
+
+
+
+        // ................... compute derivatives ................... //
+
+        // first column is the 0-order derivatives, so the shape funcitons themselves.
+        shape_ders_mat.column_mut(0).copy_from(&ndu.column(p));
+        let mut alpha_buf = [0.0; (PMAX+1)*(PMAX+1)];
+        let mut alpha = na::DMatrixViewMut::from_slice(&mut alpha_buf, n+1, n+1);
+
+        alpha[(0, 0)] = 1.0;
+        for k in 1..n+1 
+        {
+            let u1 = self.knots[sp + p - k + 1];
+            let u2 = self.knots[sp];
+            alpha[(k, 0)] = alpha[(k-1, 0)] / (u1 - u2);
+
+            for j in 1..k
+            {
+                let u3 = self.knots[sp + p + j - k + 1];
+                let u4 = self.knots[sp + j];
+                alpha[(k, j)] = (alpha[(k-1, j)] - alpha[(k-1, j-1)]) / (u3 - u4);
+            }
+
+            let u5 = self.knots[sp + p + 1];
+            let u6 = self.knots[sp + k];
+            alpha[(k, k)] = -alpha[(k-1, k-1)] / (u5 - u6);
+        }
+
+        for i in 0..p+1 // shape function index
+        {
+            for k in 1..n+1 // derivative order
+            {
+                for j in 0..k+1 // sum over shape funs
+                {
+                    shape_ders_mat[(i, k)] += alpha[(k, j)] * ndu[(j, p - k)];
+                }
+
+                let mut binom = (p - k + 1) as f64;
+                for val in (p - k + 2) .. p+1 
+                {
+                    binom *= (val as f64);
+                }
+                shape_ders_mat[(i, k)] *= binom;
+            }
+        }
 
 
     }
@@ -157,9 +252,10 @@ mod tests {
     use super::BsplineBasis;
     use super::PMAX;
 
-    use float_cmp::assert_approx_eq;
+    use approx::assert_relative_eq;
     use serde::Deserialize;
     use std::fs;
+    use nalgebra as na;
 
     #[derive(Deserialize)]
     struct ParamData{
@@ -241,10 +337,6 @@ mod tests {
                 {
                     let span1 = test_data.$span.values[idx];
                     let span2 = basis.find_span(*u, $order);
-                    if span1 != span2 
-                    {
-                        println!("idx = {} u = {} span1 = {} span2 = {}", idx, u, span1, span2);
-                    }
                     assert_eq!(span1, span2);
                 }
             }    
@@ -278,7 +370,7 @@ mod tests {
                     {
                         let val1 = basis_funs1[i];
                         let val2 = basis_funs2[i - start];
-                        assert_approx_eq!(f64, val1, val2, ulps = KNOT_ULPS);
+                        assert_relative_eq!(val1, val2, max_relative = 1e-14);
                     }
                     
                 }
@@ -291,30 +383,48 @@ mod tests {
     eval!(eval2, knots_p2, basis_p2, 2); 
     eval!(eval3, knots_p3, basis_p3, 3); 
     eval!(eval4, knots_p4, basis_p4, 4); 
-    // eval!(eval0, knots_p0, basis_p0, 0); 
+    //..............................................................................................
 
-    // #[test]
-    // fn eval() {
+    // macro_rules! eval_diff {
+    //     ($test_name:ident, $knots:ident, $ders:ident, $order:expr) => {
+    //         #[test]
+    //         fn $test_name() {
+    //             let test_data = TestData::new();
+    //             let basis = BsplineBasis::new(test_data.$knots.values.clone());
+    //             for (idx, u) in test_data.u.values.iter().enumerate()
+    //             {
+    //                 let ders_start = idx * ($order + 1);
+    //                 let ders_end = (idx+1) * ($order + 1);
+    //                 let ders1 = test_data.$ders.values[ders_start..ders_end].to_vec();
 
-    //     let test_data = TestData::new();
-    //     let basis = BsplineBasis::new(test_data.knots_p0.values.clone());
 
-    //     for (idx, u) in test_data.u.values.iter().enumerate()
-    //     {
+    //                 let mut ders_buf = [0.0; (PMAX+1) * (PMAX + 1)];
+    //                 basis.eval_diff(*u, $order, $order, &mut ders_buf);
+    //                 let ders2 = na::DMatrixViewMut::from_slice(&mut ders_buf, $order+1, $order+1);
 
-    //         let (start, end, _num_basis) = basis.non_zero_basis(*u, 0);
-    //         let basis_funs1 = test_data.basis_p0.values[idx].clone();
+                    
+    //                 let (start, _end, num_basis) = basis.non_zero_basis(*u, $order);
 
-    //         let mut basis_funs2 = [0.0; PMAX];
-    //         basis.eval(*u, 0, &mut basis_funs2);
-
-    //         for i in start..end
-    //         {
-    //             let val1 = basis_funs1[i];
-    //             let val2 = basis_funs2[i - start];
-    //             assert_approx_eq!(f64, val1, val2, ulps = KNOT_ULPS);
+    //                 // println!("........................................... {} {}", idx, u);
+    //                 for j in 0..$order+1 
+    //                 {
+    //                     for k in 0..num_basis 
+    //                     {
+    //                         let val1 = ders1[j][k + start];
+    //                         let val2 = ders2[(k, j)];
+    //                         // println!("j = {} k = {} val1 = {} val2 = {}", j, k, val1, val2);
+    //                         assert_relative_eq!(val1, val2, max_relative = 1e-14);
+    //                     }
+    //                 }
+    //             }
     //         }
-            
-    //     }
+    //     };
     // }
+
+    // eval_diff!(eval_diff0, knots_p0, ders_p0, 0);
+    // eval_diff!(eval_diff1, knots_p1, ders_p1, 1);
+    // eval_diff!(eval_diff2, knots_p2, ders_p2, 2);
+    // eval_diff!(eval_diff3, knots_p3, ders_p3, 3);
+    // eval_diff!(eval_diff4, knots_p4, ders_p4, 4);
+
 }
--- src/utl/mod.rs
+++ src/utl/mod.rs
@@ -1,8 +1,8 @@
 //! This is the UTiLities module
-//! 
-//! 
-//! 
-//! 
+//!
+//!
+//!
+//!
 
 use static_assertions::const_assert;
 
@@ -11,58 +11,108 @@ use static_assertions::const_assert;
 // ------------------------------------------- Structs ------------------------------------------ //
 
 #[repr(C)]
-pub struct LinearIndex<const N: usize> 
-{
+pub struct LinearIndex<const N: usize> {
     dims: [usize; N],
-    idx_helper: [usize; N], 
+    idx_helper: [usize; N],
     tuple_helper: [usize; N],
 }
 
 // ------------------------------------------- Enums -------------------------------------------- //
 
-
 // ------------------------------------------- Types -------------------------------------------- //
-
 
 // ----------------------------------------- Constants ------------------------------------------ //
 
-
 // --------------------------------------- Free Functions---------------------------------------- //
-
 
 // --------------------------------------- Implementations -------------------------------------- //
 
 impl<const N: usize> LinearIndex<N> {
-
     // const_assert!(N > 0);
 
-
-    pub fn new(dims: &[usize; N]) -> Self
-    {
+    pub fn new(dims: &[usize; N]) -> Self {
         assert!(dims.len() == N);
 
-        let mut lin_idx = LinearIndex{
+        let mut lin_idx = LinearIndex {
             dims: *dims,
-            idx_helper: [1; N], 
+            idx_helper: [1; N],
             tuple_helper: *dims,
         };
 
-        for i in 1..N
-        {
-            lin_idx.idx_helper[i] = lin_idx.idx_helper[i-1] * dims[i-1];
-            lin_idx.tuple_helper[i] = lin_idx.tuple_helper[i] * lin_idx.tuple_helper[i-1];
+        for i in 1..N {
+            lin_idx.idx_helper[i] = lin_idx.idx_helper[i - 1] * dims[i - 1];
+            lin_idx.tuple_helper[i] = lin_idx.tuple_helper[i] * lin_idx.tuple_helper[i - 1];
         }
         lin_idx
     }
 
-    pub fn index(&self, indices: &[usize; N])
-    {
-        let idx = 0usize;
-        for i in 0..N
-        {
-            idx += (indices[i] * )
+    pub fn index(&self, indices: &[usize; N]) -> usize {
+        let mut idx = 0usize;
+        for i in 0..N {
+            idx += (indices[i] * self.idx_helper[i]);
         }
+        idx
     }
+    //..............................................................................................
+
+    pub fn tuple(&self, idx: usize) -> [usize; N] {
+        let mut out = [0; N];
+        out[0] = idx % self.dims[0];
+        for i in 1..N {
+            let base = (idx / self.tuple_helper[i]) * self.tuple_helper[i];
+            out[i] = (idx - base) / self.tuple_helper[i - 1];
+        }
+        out
+    }
+    //..............................................................................................
 }
 
 // ------------------------------------------- Tests -------------------------------------------- //
+
+mod tests {
+    use crate::utl::LinearIndex;
+
+    #[test]
+    fn linear_index2() {
+        let lin_idx = LinearIndex::new(&[3, 4]);
+
+        let mut idx1 = 0;
+        for j in 0..4
+        {
+            for i in 0..3
+            {
+                let tuple1 = [i, j];
+                let tuple2 = lin_idx.tuple(idx1);
+                let idx2 = lin_idx.index(&tuple1);
+                assert_eq!(idx1, idx2);
+                assert_eq!(tuple1, tuple2);
+                idx1 += 1;
+            }
+        }
+    }
+
+    #[test]
+    fn linear_index3() {
+
+        let lin_idx = LinearIndex::new(&[3, 4, 2]);
+
+        let mut idx1 = 0;
+        for k in 0..2
+        {
+            for j in 0..4
+            {
+                for i in 0..3
+                {
+                    let tuple1 = [i, j, k];
+                    let tuple2 = lin_idx.tuple(idx1);
+                    let idx2 = lin_idx.index(&tuple1);
+                    assert_eq!(idx1, idx2);
+                    assert_eq!(tuple1, tuple2);
+                    idx1 += 1;
+                }
+            }
+        }
+
+    }
+
+}
