485c140824862af46172e6a61a1082009ee34785
John Ferguson <jaf13@Johns-Mac-mini.local>
2023-10-29 23:37:42 +0000

    about to implement eval_diff in new way

--- src/spl/mod.rs
+++ src/spl/mod.rs
@@ -57,6 +57,17 @@ fn upper_bound(arr: &[f64], value: f64) -> usize
     let comp = |val: &f64| if knot_lt(*val, value)  {std::cmp::Ordering::Less} else {std::cmp::Ordering::Greater};
     arr.binary_search_by(comp).unwrap_or_else(|index| index)
 }
+
+fn binom_coeff(p: usize, k: usize) -> f64
+{
+    assert!(p >= k, "p must be greater than k");
+    let mut out = (p - k + 1);
+    for i in p-k+2 .. p+1
+    {
+        out *= i;
+    }
+    out as f64
+}
 // --------------------------------------- Implementations -------------------------------------- //
 impl BsplineBasis
 {
@@ -144,100 +155,12 @@ impl BsplineBasis
 
 
 
-    pub fn eval_diff(&self, u: f64, p: usize, n: usize, shape_ders: &mut [f64])
+    pub fn eval_diff(&self, u: f64, p: usize, k: usize, shape_ders: &mut [f64])
     {
         assert!(self.is_member(u), "u is not member of parameter range");
         assert!(p <= PMAX, "order exceeds max allowable");
-        assert!(n <= p, "order of derivative exceeds order");
-        assert!(shape_ders.len() >= (p+1)*(n+1), "Derivative buffer too small");
-
-        shape_ders.fill(0.0);
-        let mut shape_ders_mat = na::DMatrixViewMut::from_slice(shape_ders, n+1, p+1);
-
-        let mut ndu_buf = [0.0; (PMAX+1) * (PMAX+1)];
-        let mut ndu = na::DMatrixViewMut::from_slice(&mut ndu_buf, p+1, p+1);
-
-
-        // ................... compute knot differences  ................... //
-        let sp = self.find_span(u, p);
-        let (mut saved, mut temp) = (0.0, 0.0);
-        let mut left = [0.0; PMAX + 1];
-        let mut right = [0.0; PMAX + 1];
-
-        /* doc: explanation of ndu
-        Upper triangle of ndu is the shape functions, so ndu[(0, 0)]
-        contains N_{0,0}, ndu[(0, 1)] = N{0, 1}, ndu[(1, 1)] = N{1, 1} 
-        and so on. 
-
-        Lower triagle of ndu are the knot differences, so 
-        ndu[(1,0)] = u_{i+1} - u_{i}
-        ndu[(2,0)] = u_{i+1} - 
-        */
-        ndu[(0, 0)] = 1.0;
-
-        for j in 1..p+1 
-        {
-            saved = 0.0;
-            left[j] = u - self.knots[sp + 1 - j];
-            right[j] = self.knots[sp + j] - u;
-
-            for r in 0..j
-            {
-                ndu[(j, r)] = right[r + 1] + left[j - r];
-                temp = ndu[(r, j - 1)] / ndu[(j, r)];
-
-                ndu[(r, j)] = saved + (right[r+1] * temp);
-                saved = left[j - r] * temp;
-            }
-            ndu[(j, j)] = saved;
-        }
-
-
-
-        // ................... compute derivatives ................... //
-
-        // first column is the 0-order derivatives, so the shape funcitons themselves.
-        shape_ders_mat.column_mut(0).copy_from(&ndu.column(p));
-        let mut alpha_buf = [0.0; (PMAX+1)*(PMAX+1)];
-        let mut alpha = na::DMatrixViewMut::from_slice(&mut alpha_buf, n+1, n+1);
-
-        alpha[(0, 0)] = 1.0;
-        for k in 1..n+1 
-        {
-            let u1 = self.knots[sp + p - k + 1];
-            let u2 = self.knots[sp];
-            alpha[(k, 0)] = alpha[(k-1, 0)] / (u1 - u2);
-
-            for j in 1..k
-            {
-                let u3 = self.knots[sp + p + j - k + 1];
-                let u4 = self.knots[sp + j];
-                alpha[(k, j)] = (alpha[(k-1, j)] - alpha[(k-1, j-1)]) / (u3 - u4);
-            }
-
-            let u5 = self.knots[sp + p + 1];
-            let u6 = self.knots[sp + k];
-            alpha[(k, k)] = -alpha[(k-1, k-1)] / (u5 - u6);
-        }
-
-        for i in 0..p+1 // shape function index
-        {
-            for k in 1..n+1 // derivative order
-            {
-                for j in 0..k+1 // sum over shape funs
-                {
-                    shape_ders_mat[(i, k)] += alpha[(k, j)] * ndu[(j, p - k)];
-                }
-
-                let mut binom = (p - k + 1) as f64;
-                for val in (p - k + 2) .. p+1 
-                {
-                    binom *= (val as f64);
-                }
-                shape_ders_mat[(i, k)] *= binom;
-            }
-        }
-
+        assert!(k <= p, "order of derivative exceeds order");
+        assert!(shape_ders.len() >= (p+1), "Derivative buffer too small");
 
     }
 }
@@ -387,46 +310,42 @@ mod tests {
     eval!(eval4, knots_p4, basis_p4, 4); 
     //..............................................................................................
 
-    // macro_rules! eval_diff {
-    //     ($test_name:ident, $knots:ident, $ders:ident, $order:expr) => {
-    //         #[test]
-    //         fn $test_name() {
-    //             let test_data = TestData::new();
-    //             let basis = BsplineBasis::new(test_data.$knots.values.clone());
-    //             for (idx, u) in test_data.u.values.iter().enumerate()
-    //             {
-    //                 let ders_start = idx * ($order + 1);
-    //                 let ders_end = (idx+1) * ($order + 1);
-    //                 let ders1 = test_data.$ders.values[ders_start..ders_end].to_vec();
+    macro_rules! eval_diff {
+        ($test_name:ident, $knots:ident, $ders:ident, $order:expr) => {
+            #[test]
+            fn $test_name() {
+                let p = $order;
+                let test_data = TestData::new();
+                let basis = BsplineBasis::new(test_data.$knots.values.clone());
+                for (idx, u) in test_data.u.values.iter().enumerate()
+                {
+                    let ders_start = idx * (p + 1);
+                    let ders_end = (idx+1) * (p + 1);
+                    let ders1_all = test_data.$ders.values[ders_start..ders_end].to_vec();
 
+                    let (start, _end, num_basis) = basis.non_zero_basis(*u, p);
 
-    //                 let mut ders_buf = [0.0; (PMAX+1) * (PMAX + 1)];
-    //                 basis.eval_diff(*u, $order, $order, &mut ders_buf);
-    //                 let ders2 = na::DMatrixViewMut::from_slice(&mut ders_buf, $order+1, $order+1);
+                    for j in 0..p+1 
+                    {
+                        let ders1 = ders1_all[j].clone();
+                        let mut ders2 = [0.0; PMAX+1];
+                        basis.eval_diff(*u, p, j, &mut ders2);
+                        for k in 0..num_basis 
+                        {
+                            let val1 = ders1[k + start];
+                            let val2 = ders2[k];
+                            assert_relative_eq!(val1, val2, max_relative = 1e-14);
+                        }
+                    }
+                }
+            }
+        };
+    }
 
-                    
-    //                 let (start, _end, num_basis) = basis.non_zero_basis(*u, $order);
-
-    //                 // println!("........................................... {} {}", idx, u);
-    //                 for j in 0..$order+1 
-    //                 {
-    //                     for k in 0..num_basis 
-    //                     {
-    //                         let val1 = ders1[j][k + start];
-    //                         let val2 = ders2[(k, j)];
-    //                         // println!("j = {} k = {} val1 = {} val2 = {}", j, k, val1, val2);
-    //                         assert_relative_eq!(val1, val2, max_relative = 1e-14);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     };
-    // }
-
-    // eval_diff!(eval_diff0, knots_p0, ders_p0, 0);
-    // eval_diff!(eval_diff1, knots_p1, ders_p1, 1);
-    // eval_diff!(eval_diff2, knots_p2, ders_p2, 2);
-    // eval_diff!(eval_diff3, knots_p3, ders_p3, 3);
-    // eval_diff!(eval_diff4, knots_p4, ders_p4, 4);
+    eval_diff!(eval_diff0, knots_p0, ders_p0, 0);
+    eval_diff!(eval_diff1, knots_p1, ders_p1, 1);
+    eval_diff!(eval_diff2, knots_p2, ders_p2, 2);
+    eval_diff!(eval_diff3, knots_p3, ders_p3, 3);
+    eval_diff!(eval_diff4, knots_p4, ders_p4, 4);
 
 }
